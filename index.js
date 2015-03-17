dec75f71daab2cd6a310bca1d1535165ac3e811e
Roman Shtylman <shtylman@gmail.com>
2015-03-16 17:37:09 -0700

    refactor to allow for bust requests
    Without bursting, this module behaves like a basic `throttle` and limits
    execution of the function to limitCount/limitInterval ms between calls.
    
    This change introduces the ability to burst and actually allows up to
    limitCount function calls within the limitInterval

--- index.js
+++ index.js
@@ -1,57 +1,44 @@
 module.exports = rateLimit;
 
 function rateLimit(limitCount, limitInterval, fn) {
-  var fifo = []
-    , callCount = 0
-    , startTime = new Date().getTime()
-    ;
-  
-  return executeRateLimitedFunction;
-  
-  function executeRateLimitedFunction () {
-    var args
-      , now
-      , rate
-      ;
-    
-    args = Array.prototype.slice.call(arguments);
-    if (!startTime) startTime = new Date().getTime();
-    now = new Date().getTime();
-    
-    if (now - startTime > limitInterval) {
-      //if the time passed exceeds the limitInterval then reset the startTime 
-      //and callCount. This allows us to recalculate the rate for each 
-      //limitInterval
-      
-      startTime = now;
-      callCount = 0;
-    }
-    
-    rate = (callCount) / (now - startTime);
-    
-    if (rate > (limitCount / limitInterval)) {
-      // we have exceeded the rate we need to start queuing
-      if (args.length) {
-        //we exceeded the rate limit when attempting to actually
-        //executeRateLimitedFunction, so we need to push the fn onto the fifo
-        fifo.push(args);
+  var fifo = [];
+
+  // count starts at limit
+  // each call of `fn` decrements the count
+  // it is incremented after limitInterval
+  var count = limitCount;
+
+  function call_next(args) {
+    setTimeout(function() {
+      if (fifo.length > 0) {
+        call_next();
       }
-      
-      setTimeout(executeRateLimitedFunction, limitInterval/limitCount);
-    }
-    else if (fifo.length) {
-      if (args.length) {
-        //be sure to push the current call on to the queue
-        //if we had the specified args.
-        fifo.push(args);
+      else {
+        count = count + 1;
       }
-      
-      process.nextTick(fn.bind.apply(fn, [fn].concat(fifo.shift())));
-      callCount += 1;
+    }, limitInterval);
+
+    var call_args = fifo.shift();
+
+    // if there is no next item in the queue
+    // and we were called with args, trigger function immediately
+    if (!call_args && args) {
+      fn.apply(args[0], args[1]);
+      return;
     }
-    else {
-      process.nextTick(fn.bind.apply(fn, [fn].concat(args)));
-      callCount += 1;
+
+    fn.apply(call_args[0], call_args[1]);
+  }
+
+  return function rate_limited_function() {
+    var ctx = this;
+    var args = Array.prototype.slice.call(arguments);
+    if (count <= 0) {
+      fifo.push([ctx, args]);
+      return;
     }
+
+    count = count - 1;
+    call_next([ctx, args]);
   };
 }
